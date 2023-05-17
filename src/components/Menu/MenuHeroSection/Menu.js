144f64bdde351c9cb49047030169a581bc1eb402
numixious <numixious@gmail.com>
2023-05-17 13:22:19 +0430

    Menu added

--- src/components/Menu/MenuHeroSection/Menu.js
+++ src/components/Menu/MenuHeroSection/Menu.js
@@ -7,6 +7,24 @@ const Menu = () => {
     <section className={defaults.container}>
       <div className={styles.menu}>
         <h1>منوی رستوران</h1>
+        <div className={styles.menuFlex}>
+          <div className={styles.menuBoxes}>
+            <img src={require("./img/image-1.png")} alt="logo" />
+            <div className={styles.foodTitle}>دسر</div>
+          </div>
+          <div className={styles.menuBoxes}>
+            <img src={require("./img/image-2.png")} alt="logo" />
+            <div className={styles.foodTitle}>پیش غذا</div>
+          </div>
+          <div className={styles.menuBoxes}>
+            <img src={require("./img/image-3.png")} alt="logo" />
+            <div className={styles.foodTitle}>غذای اصلی</div>
+          </div>
+          <div className={styles.menuBoxes}>
+            <img src={require("./img/image.png")} alt="logo" />
+            <div className={styles.foodTitle}>نوشیدنی</div>
+          </div>
+        </div>
       </div>
     </section>
   );
